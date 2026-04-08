5aa743e7b009982b5ee5d0cfd2d916687130b3fe
AoIcarus <davidveridiano@gmail.com>
2026-04-08 16:57:36 +0900

    Update: Patched Brute Force Vulnerability

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+
+if( !isset( $session[ 'attempts' ] ) ) {
+    $session[ 'attempts' ] = 0;
+}
 
 if( isset( $_GET[ 'Login' ] ) ) {
 	// Get username
@@ -8,21 +13,28 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = $_GET[ 'password' ];
 	$pass = md5( $pass );
 
-	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
-
+	if ($session['attempts'] >= 5) {
+    	die("Too many login attempts. Try again later.");
+	}
+	else {
+		// Check the database
+		$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+		$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	}
+	
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
 		$row    = mysqli_fetch_assoc( $result );
 		$avatar = $row["avatar"];
 
 		// Login successful
+		$session['attempts'] = 0;
 		$html .= "<p>Welcome to the password protected area {$user}</p>";
 		$html .= "<img src=\"{$avatar}\" />";
 	}
 	else {
 		// Login failed
+		$session['attempts'] += 1;
 		$html .= "<pre><br />Username and/or password incorrect.</pre>";
 	}
 
